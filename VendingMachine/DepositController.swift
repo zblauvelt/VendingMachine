22e910aae3a18bf6bc3c3f83dc8309f330e4ed8b
zblauvelt <zblauvelt@hotmail.com>
2017-08-19 13:07:11 -0400

    finished-lesson

---
+++ VendingMachine/DepositController.swift
@@ -0,0 +1,29 @@
+//
+//  DepositController.swift
+//  VendingMachine
+//
+//  Created by Zachary Blauvelt on 8/19/17.
+//  Copyright © 2017 Treehouse Island, Inc. All rights reserved.
+//
+
+import UIKit
+
+class DepositController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func dismiss(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+
+}
--- VendingMachine/VendingMachine.swift
+++ VendingMachine/VendingMachine.swift
@@ -136,7 +136,7 @@ class FoodVendingMachine: VendingMachine {
     }
     
     func deposit(_ amount: Double) {
-        
+        amountDeposited += amount
     }
     
     func item(forSelection selection: VendingSelection) -> VendingItem? {
